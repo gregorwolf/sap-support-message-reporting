a819d08952c98b7afc74c37a34eda6adf34332d7
Gregor Wolf <gregor.wolf@gmail.com>
2021-02-27 17:25:17 +0100

    chore: refactor cleanObject

---
+++ srv/helper.js
@@ -0,0 +1,24 @@
+const { format } = require("date-fns");
+
+function cleanObject(obj) {
+  Object.keys(obj).forEach((key) => {
+    const value = obj[key];
+    if (typeof value === "object") {
+      delete obj[key];
+    } else if (typeof value === "string") {
+      // match the common sap odata date format like "/Date(1603065600000)/"
+      // also matches the more uncommon "/Date(253402300799000+0000)/". But this will only work correctly for utc! (+0000)
+      const matchDate = value.match(/\/Date\((-*\d+)\+*\d+\)\//);
+      if (matchDate) {
+        obj[key] = format(new Date(Number(matchDate[1])), "yyyy-MM-dd");
+      }
+      // match the sap odata time format like "PT09H50M58S"
+      const matchTime = value.match(/^PT(\d\d)H(\d\d)M(\d\d)S/);
+      if (matchTime) {
+        obj[key] = matchTime[1] + ":" + matchTime[2] + ":" + matchTime[3] + ".00";
+      }
+    }
+  });
+}
+
+exports.cleanObject = cleanObject
--- srv/init.js
+++ srv/init.js
@@ -1,5 +1,5 @@
-const cds = require("@sap/cds");
-const { format } = require("date-fns");
+const cds = require("@sap/cds")
+const { cleanObject } = require("./helper")
 
 const thisApplication = "sap-support-message-reporting";
 const DEBUG = cds.debug(thisApplication);
@@ -26,24 +26,3 @@ module.exports = (db) => {
     cds.run([INSERT.into(key).rows(value.d.results)]);
   });
 };
-
-function cleanObject(obj) {
-  Object.keys(obj).forEach((key) => {
-    const value = obj[key];
-    if (typeof value === "object") {
-      delete obj[key];
-    } else if (typeof value === "string") {
-      // match the common sap odata date format like "/Date(1603065600000)/"
-      // also matches the more uncommon "/Date(253402300799000+0000)/". But this will only work correctly for utc! (+0000)
-      const matchDate = value.match(/\/Date\((-*\d+)\+*\d+\)\//);
-      if (matchDate) {
-        obj[key] = format(new Date(Number(matchDate[1])), "yyyy-MM-dd");
-      }
-      // match the sap odata time format like "PT09H50M58S"
-      const matchTime = value.match(/^PT(\d\d)H(\d\d)M(\d\d)S/);
-      if (matchTime) {
-        obj[key] = matchTime[1] + ":" + matchTime[2] + ":" + matchTime[3] + ".00";
-      }
-    }
-  });
-}
