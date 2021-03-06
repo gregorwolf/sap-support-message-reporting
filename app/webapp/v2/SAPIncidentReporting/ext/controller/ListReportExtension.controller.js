de3460e6b84ad921cdfc39d4f2d69b3fbfe02600
Gregor Wolf <gregor.wolf@gmail.com>
2021-03-06 21:54:07 +0100

    chore: remove manual ALP

--- app/webapp/v2/SAPIncidentReporting/ext/controller/ListReportExtension.controller.js
+++
@@ -1,5 +0,0 @@
-sap.ui.controller("v2.SAPIncidentReporting.ext.controller.ListReportExtension", {
-  onInit: function () { 
-    this.getView().byId("listReport").setProperty("useExportToExcel", true) 
-  }
-});
