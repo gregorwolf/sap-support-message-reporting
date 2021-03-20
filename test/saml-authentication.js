48fc27e72de6347805251c2769768d6e789aa85c
Gregor Wolf <gregor.wolf@gmail.com>
2021-03-20 16:15:51 +0100

    cert/key path via environment variable

--- test/saml-authentication.js
+++ test/saml-authentication.js
@@ -18,6 +18,18 @@ const logger = pino({
   prettyPrint: prettyPrint,
 });
 
+// Add Certificate for Authentication
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+  cert: fs.readFileSync(
+    process.env.HTTPS_CLIENT_CERT || "/Users/gwolf/Downloads/S0001142741.pem"
+  ),
+  key: fs.readFileSync(
+    process.env.HTTPS_CLIENT_KEY ||
+      "/Users/gwolf/Downloads/S0001142741-decrypted.key"
+  ),
+});
+
 async function authStep1(parameters) {
   try {
     const response = await axios({
@@ -34,12 +46,6 @@ async function authStep1(parameters) {
 
 async function authStep2(parameters) {
   try {
-    // Add Certificate for Authentication
-    const httpsAgent = new https.Agent({
-      rejectUnauthorized: false,
-      cert: fs.readFileSync("/Users/gwolf/Downloads/S0001142741.pem"),
-      key: fs.readFileSync("/Users/gwolf/Downloads/S0001142741-decrypted.key"),
-    });
     const response = await axios({
       httpsAgent,
       method: "POST",
@@ -98,7 +104,6 @@ async function authStep4(parameters) {
       },
       data: qs.stringify(parameters.postParameters),
     });
-    console.log(response.data);
     return processResponse(response);
   } catch (error) {
     logger.error(error);
@@ -124,6 +129,7 @@ function processResponse(response) {
   // Read the input fields with their values
   var inputFields = $("input");
   var parameters = {
+    onload: $("body").attr("onload"),
     actionURL: $("form").attr("action"),
     cookies: response.headers["set-cookie"],
     response: response,
@@ -142,7 +148,8 @@ function processResponse(response) {
 async function getFromLaunchpad(path) {
   try {
     var url = `https://launchpad.support.sap.com${path}`;
-    logger.info("Starting request to: ", url);
+    logger.info("Starting request to:");
+    logger.info(url);
     const response = await axios({
       method: "GET",
       url,
@@ -152,10 +159,15 @@ async function getFromLaunchpad(path) {
     // indicates that we need to authenticate
     if (response.headers["com.sap.cloud.security.login"] === "login-request") {
       resultStep0 = processResponse(response);
-      logger.info("Response body from Step 0");
-      logger.info(resultStep0.response.data);
-      logger.info("Response headers from Step 0");
-      logger.info(resultStep0.response.headers);
+      // the HTML Source countains an onload event where a cookie is set in the browser via Javascript
+      var parseOnload = resultStep0.onload.split("cookie='")[1].split("=")[0];
+      var browserCookie = parseOnload + "=" + encodeURIComponent(url);
+      logger.debug("browserCookie from Step 0");
+      logger.debug(browserCookie);
+      logger.debug("Response body from Step 0");
+      logger.debug(resultStep0.response.data);
+      logger.debug("Response headers from Step 0");
+      logger.debug(resultStep0.response.headers);
       logger.info("Authentication Step 1 via:");
       logger.info(resultStep0.actionURL);
       const resultStep1 = await authStep1(resultStep0);
@@ -168,10 +180,14 @@ async function getFromLaunchpad(path) {
       logger.info("Authentication Step 3 via:");
       logger.info(resultStep2.actionURL);
       const resultStep3 = await authStep3(resultStep2);
-      // The cookie from Step 0 plus the sap.com Cookie from Step 2 must be passed to Step 4
+      // The cookie from Step 0 including the browserCookie plus the sap.com Cookie from Step 2 must be passed to Step 4
       let IDP_SESSION_MARKER_accounts = prepareCookie([resultStep2.cookies[2]]);
       resultStep3.cookie =
-        resultStep0.cookie + "; " + IDP_SESSION_MARKER_accounts;
+        resultStep0.cookie +
+        "; " +
+        IDP_SESSION_MARKER_accounts +
+        "; " +
+        browserCookie;
       logger.info("Authentication Step 4 via:");
       logger.info(resultStep3.actionURL);
       logger.debug("Cookie");
