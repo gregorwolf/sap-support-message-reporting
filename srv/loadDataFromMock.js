5e30b035c057d8d9875a09048f677eb65894cd88
Gregor Wolf <gregor.wolf@gmail.com>
2024-02-01 18:01:32 +0100

    WIP: task for data load

---
+++ srv/loadDataFromMock.js
@@ -0,0 +1,24 @@
+const cds = require("@sap/cds");
+const LOG = cds.log("task-load-data");
+
+async function loadDataFromMock() {
+  var test = await cds.test();
+
+  const reportingService = await cds.connect.to("ReportingService");
+  const { MessageHeaderSet } = reportingService.entities;
+  LOG.info("Connected to ReportingService");
+  // Check that the db connection is working
+  const messageHeaderSetResult = await SELECT("Pointer")
+    .from(MessageHeaderSet)
+    .limit(1);
+  LOG.info("messageHeaderSetResult", messageHeaderSetResult);
+  // Load the data from the mock
+  const loadDataFromMockResult = await reportingService.send(
+    "loadDataFromMock"
+  );
+  LOG.info("loadDataFromMockResult", loadDataFromMockResult);
+  await cds.exit();
+  return;
+}
+
+loadDataFromMock();
