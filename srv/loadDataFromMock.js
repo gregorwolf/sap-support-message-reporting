63020e7b3d71dd365cbce4d7b6be5b41595d70e1
Gregor Wolf <gregor.wolf@gmail.com>
2024-02-02 16:30:13 +0100

    start CAP headless

--- srv/loadDataFromMock.js
+++ srv/loadDataFromMock.js
@@ -2,9 +2,21 @@ const cds = require("@sap/cds");
 const LOG = cds.log("task-load-data");
 
 async function loadDataFromMock() {
-  // start for development
-  // var test = await cds.test("", "--with-mocks");
-  var test = await cds.test();
+  const app = cds.app;
+  cds.emit("bootstrap", app);
+
+  // load model from all sources
+  const csn = await cds.load("*");
+  cds.model = cds.compile.for.nodejs(csn);
+  cds.emit("loaded", cds.model);
+
+  // connect to prominent required services
+  if (cds.requires.db) cds.db = await cds.connect.to("db");
+  if (cds.requires.messaging) await cds.connect.to("messaging");
+
+  // serve own services as declared in model
+  await cds.serve("all").from(csn).in(app);
+  await cds.emit("served", cds.services);
 
   const reportingService = await cds.connect.to("ReportingService");
   const { MessageHeaderSet } = reportingService.entities;
